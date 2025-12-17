771ea2e3ef9e759fa3437afa29ad6c263e8ed686
Slava <olor.guard@gmail.com>
2025-12-17 23:07:28 +0600

    bsm fix 7

--- src/server/trpc/routers/auth.ts
+++ src/server/trpc/routers/auth.ts
@@ -71,14 +71,14 @@ export const authRouter = router({
 
       const inserted = await supabase
         .from("users")
-        .insert<UserInsert>([
+        .insert([
           {
             email,
             username,
             display_name: username,
             is_admin: false,
             password_hash,
-          },
+          } satisfies UserInsert,
         ])
         .select(publicColumns)
         .single();
